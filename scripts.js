e00b5484de8fa2f85eb3376d78ca299a9bfd4553
nishchay-tushir <instatushir@gmail.com>
2023-07-23 15:30:09 +0530

    Initial Upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const listToChangeColor = document.querySelector('.change-color');
+    const linksToChangeColor = listToChangeColor.querySelectorAll('li a');
+  
+    if (scrollPosition >= 600) {
+      listToChangeColor.classList.add('scrolled');
+      linksToChangeColor.forEach(link => link.classList.add('scrolled'));
+    } else {
+      listToChangeColor.classList.remove('scrolled');
+      linksToChangeColor.forEach(link => link.classList.remove('scrolled'));
+    }
+  });
+  
+  function toggleNavbar() {
+          var navbarLinks = document.getElementById("listitem");
+          navbarLinks.classList.toggle("show");
+          }
+          
+  
