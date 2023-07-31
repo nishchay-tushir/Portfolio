c38ad6e8a300aae5ae8820074d1ccd394bce1320
nishchay-tushir <instatushir@gmail.com>
2023-07-31 20:58:52 +0530

    Skill section added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const listToChangeColor = document.querySelector('.change-color');
+    const nav = document.querySelector('.whitepart');
+    const popin = document.querySelector('.main');
     const linksToChangeColor = listToChangeColor.querySelectorAll('li a');
   
-    if (scrollPosition >= 500) {
+    if (scrollPosition >= 700) {
       listToChangeColor.classList.add('scrolled');
+      nav.classList.add('scrolled');
+      popin.classList.add('scrolled');
       linksToChangeColor.forEach(link => link.classList.add('scrolled'));
     } else {
       listToChangeColor.classList.remove('scrolled');
+      popin.classList.remove('scrolled');
+      nav.classList.remove('scrolled');
       linksToChangeColor.forEach(link => link.classList.remove('scrolled'));
     }
   });
